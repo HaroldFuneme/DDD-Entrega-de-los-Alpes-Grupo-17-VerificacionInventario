9e0b8dd9086ea485ae305178c08f67caa781ca83
jpfeliciano <jp.feliciano10@uniandes.edu.co>
2023-03-15 01:19:50 -0500

    Implementación de verificación de inventario y publicación de evento para despachar orden.

--- init.sql
+++ init.sql
@@ -7,17 +7,17 @@ values (uuid(), 'Bodega 2', 'Calle 321 # 45 - 6', 'Medellín', 'BODEGA');
 insert into bodegas (id, nombre_bodega, direccion, ciudad, tipo)
 values (uuid(), 'Centro de Distribución 1', 'Calle 132 # 45 - 6', 'Bogotá', 'CENTRO_DISTRIBUCION');
 
-insert into inventario_bodega (bodega_id, item_id)
-values ((select id from bodegas where nombre_bodega = 'Bodega 1'), 'item1');
+insert into inventario_bodega (bodega_id, item_id, cantidad_disponible)
+values ((select id from bodegas where nombre_bodega = 'Bodega 1'), 'item1',2);
 
-insert into inventario_bodega (bodega_id, item_id)
-values ((select id from bodegas where nombre_bodega = 'Bodega 2'), 'item1');
+insert into inventario_bodega (bodega_id, item_id, cantidad_disponible)
+values ((select id from bodegas where nombre_bodega = 'Bodega 2'), 'item1',1);
 
-insert into inventario_bodega (bodega_id, item_id)
-values ((select id from bodegas where nombre_bodega = 'Bodega 1'), 'item3');
+insert into inventario_bodega (bodega_id, item_id, cantidad_disponible)
+values ((select id from bodegas where nombre_bodega = 'Bodega 1'), 'item3',0);
 
-insert into inventario_bodega (bodega_id, item_id)
-values ((select id from bodegas where nombre_bodega = 'Bodega 2'), 'item2');
+insert into inventario_bodega (bodega_id, item_id, cantidad_disponible)
+values ((select id from bodegas where nombre_bodega = 'Bodega 2'), 'item2',1);
 
-insert into inventario_bodega (bodega_id, item_id)
-values ((select id from bodegas where nombre_bodega = 'Centro de Distribución 1'), 'item4');
+insert into inventario_bodega (bodega_id, item_id, cantidad_disponible)
+values ((select id from bodegas where nombre_bodega = 'Centro de Distribución 1'), 'item4',2);
